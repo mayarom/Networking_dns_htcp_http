cd1c9cb81283fb66bc289e62d67242f5235dc942
mayarom <95377680+mayarom@users.noreply.github.com>
2023-03-12 08:53:39 +0200

    Update client.py

--- Part_b/client.py
+++ Part_b/client.py
@@ -1,35 +1,27 @@
 import socket
 
-# Set the server address and port
+# Set up the server address and port
 SERVER_ADDR = 'localhost'
 SERVER_PORT = 20251
 
-# Create a TCP socket
+# Set up the maximum segment size for the connection
+MSS = 1024
+
+# Set up the client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
+# Connect the socket to the server address and port
 client_socket.connect((SERVER_ADDR, SERVER_PORT))
 
-# Send a request to the server
-request = b'GET /finalhtml/Home.html HTTP/1.1\r\nHost: localhost\r\n\r\n'
-client_socket.sendall(request)
+# Send a request to the server for the homepage
+request = 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n'
+client_socket.send(request.encode('utf-8'))
 
-# Receive a response from the server
-response = b''
-while True:
-    data = client_socket.recv(1024)
-    if not data:
-        break
-    response += data
+# Receive the server's response
+response = client_socket.recv(MSS).decode('utf-8')
 
-# Check if the response includes headers
-if b'\r\n\r\n' in response:
-    headers, content = response.split(b'\r\n\r\n', 1)
-    print(content.decode())
-else:
-    # If the response does not include headers, assume that the entire response is the content
-    print(response.decode())
+# Print the response
+print(response)
 
 # Close the socket
-print('Closing socket...')
 client_socket.close()
